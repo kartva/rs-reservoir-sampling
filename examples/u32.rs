29ba8d7af147816a42c8010ae6774d8879458fb8
Kartavya Vashishtha <kartavya0703@gmail.com>
2020-12-22 21:26:19 +0530

    Corrected example requiring passing &u32

--- examples/u32.rs
+++ examples/u32.rs
@@ -3,9 +3,8 @@ use reservoir_sampling::unweighted::{l, /*r*/};
 fn main () {
     let arr = (0u32..100).collect::<Vec<u32>>();
 
-    // Working with borrows is necessary as r does not expect T to implement Copy.
-    let mut sampled_arr = vec![&0u32; 10];
-    l(arr.iter(), &mut sampled_arr[0..]);
+    let mut sampled_arr = vec![0u32; 10];
+    l(arr.iter().copied(), &mut sampled_arr[..]);
     println!("With Algorithm L: {:?}", sampled_arr);
 /*
     r(arr.iter(), &mut sampled_arr[0..]);
